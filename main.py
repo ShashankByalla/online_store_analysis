451b63ce7c0514bd9d4bbfeff296295e887aaa37
Shashank <106898783+ShashankByalla@users.noreply.github.com>
2024-07-31 00:50:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def compute_revenue_by_month(df):
     df['month'] = df['order_date'].dt.to_period('M')
-    return df.groupby('month')['product_price', 'quantity'].apply(lambda x: (x['product_price'] * x['quantity']).sum()).reset_index(name='revenue').set_index('month')['revenue']
+    return df.groupby('month').apply(lambda x: (x['product_price'] * x['quantity']).sum()).reset_index(name='revenue').set_index('month')['revenue']
 
 def compute_revenue_by_product(df):
     return df.groupby('product_name', group_keys=False).apply(lambda x: (x['product_price'] * x['quantity']).sum()).reset_index(name='revenue').set_index('product_name')['revenue']
